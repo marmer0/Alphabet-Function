747f2d2d10f1f9424bca4722dbe519358b6200d2
Maddy Armer <madeleinearmer@gmail.com>
2021-11-09 19:43:35 -0600

    Uploading my package!

---
+++ R/alph.R
@@ -0,0 +1,7 @@
+alph <-
+function(){
+  letters <- c("a","b", "c","d","e", "f","g", "h", "i", "j","k",
+               "l","m","n","o","p","q","r","s","t","u","v", "w", "x", "y", "z")
+  samp <- sample(letters, 1)
+  return(samp)
+}
